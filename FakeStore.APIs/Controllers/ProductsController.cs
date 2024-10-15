f4895facfc0e21cca506e61c5c0815940fe6b96e
Abdelrahman Mohamed Younis <younisabdo4@gmail.com>
2024-10-16 02:17:45 +0300

    refactor: ProductController

--- FakeStore.APIs/Controllers/ProductsController.cs
+++ FakeStore.APIs/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using FakeStore.Core.Models;
 using FakeStore.Core.Repositories;
+using FakeStore.Core.Specifications;
 using FakeStore.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,14 +21,16 @@ namespace FakeStore.APIs.Controllers
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
 
-            var Products = await _repository.GetAllAsync();
+            var Spec = new ProductBrandTypeSpecification();
+            var Products = await _repository.GetAllWithSpecificationAsync(Spec);
             return Ok(Products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var Product = await _repository.GetByIdAsync(id);
+            var Spec = new ProductBrandTypeSpecification(id);
+            var Product = await _repository.GetByIdWithSpecificationAsync(Spec);
             return Ok(Product);
         }
 
