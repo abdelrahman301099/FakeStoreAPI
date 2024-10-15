3f17ac178bf76d7cf81b19b6917863b3dc3f2c8e
Abdelrahman Mohamed Younis <younisabdo4@gmail.com>
2024-10-16 02:17:01 +0300

    feat: add Spcification for Product

---
+++ FakeStore.Core/Specifications/ProductBrandTypeSpecification.cs
@@ -0,0 +1,25 @@
+﻿using FakeStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeStore.Core.Specifications
+{
+    public class ProductBrandTypeSpecification:BaseSpecifications<Product>
+    {
+        //CTOP which used to get all products 
+        public ProductBrandTypeSpecification() : base()
+        {
+            Includes.Add(p => p.ProductBrand);
+            Includes.Add(p => p.ProductType);
+        }
+
+        public ProductBrandTypeSpecification(int id) : base(p=> p.Id == id )
+        {
+            Includes.Add(p => p.ProductBrand);
+            Includes.Add(p => p.ProductType);
+        }
+    }
+}
