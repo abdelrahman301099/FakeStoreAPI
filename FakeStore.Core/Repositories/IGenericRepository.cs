f2940f12dfcd702be30fa3eed47fbd79ea03e0c8
Abdelrahman Mohamed Younis <younisabdo4@gmail.com>
2024-10-15 15:26:12 +0300

    feat: create IGenericRepository interface and GenericRepository Class

---
+++ FakeStore.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using FakeStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeStore.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity // عشلن اضمن ان ال T دي هاتكون حاجه من نوع بيز انتيتي او حاجه بتورث منها و كدا ااكون ضامن ان اي حاجه من دول هاتكون موديل 
+    {
+        //Get All
+        Task<IEnumerable<T>> GetAllAsync();
+
+        //Get By Id
+        Task<T> GetByIdAsync(int id);
+
+
+    }
+}
---
+++ FakeStore.Repository/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using FakeStore.Core.Models;
+using FakeStore.Core.Repositories;
+using FakeStore.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeStore.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly FakeStoreDbContext _dbContext;
+        public GenericRepository(FakeStoreDbContext dbContext)//Ask CLR to create object from the context Implicitly
+        {
+            _dbContext = dbContext;
+            
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+
+            //return await _dbContext.Set<T>().Where(x=> x.Id == id).FirstOrDefaultAsync();
+
+            return await _dbContext.Set<T>().FindAsync(id);//Search Localy First
+        }
+    }
+}
