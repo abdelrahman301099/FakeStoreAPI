3063877ce87f3d6cb15da266dda9e816f7c57349
Abdelrahman Mohamed Younis <younisabdo4@gmail.com>
2024-10-15 15:52:29 +0300

    feat:add APIController which have the common endPoints

---
+++ FakeStore.APIs/Controllers/APIController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeStore.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        //All Common EndPoints
+
+    }
+}
